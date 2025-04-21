1c359892f63d41441f963d6971465abf6445e1a2
mithat720 <mithat72@gmail.com>
2025-04-21 22:08:08 +0200

    w2-q1 is updated.
    w2-q1 is updated.

--- w2-q1.py
+++ w2-q1.py
@@ -1,57 +1,31 @@
 students ={}
-
 for i in range (0,3):
-
     student = {input("Name, Surname? "):[int(input("midterm ")),int(input("final ")), int(input("oral "))]}
-
     students.update(student)
-
     print(students)
-
 for i in students:
-
     students[i].append(round(sum(students[i])/3))
-
 print(students)
-
 avg=[]
-
 for i in students:
-
     avg.append((students[i])[-1])
-
-print("Biggest average grade: ",i, max(avg))
-
+avg=[((students[i])[-1]) for i in students]
+for i in students:
+    if (students[i])[-1]==max(avg):
+        print("Biggest average grade: ",i, ",", max(avg))
 Names=[]
-
-print(students.keys())
-
 for i in list(students.keys()):
-
     i.split(" ")[0]
-
     Names.append(i.split(" ")[0])
-
 print(tuple(Names))
-
-print("sorted names: ", sorted(Names))
-
+print("sorted names: ",sorted(Names))
 Unseccesfull=[]
-
 for i in students:
-
    if (students[i])[-1]<70:
-
     Unseccesfull.append(i)
-
 print(set(Unseccesfull))
-
 Unseccesfull=set(Unseccesfull)
-
 for i in students:
-
    if (students[i])[-1]<70:
-
-    Unseccesfull.add(i)
-
+        Unseccesfull.add(i)
 print("Unseccesfull students: ",Unseccesfull)
